f183128511d04b57b299ff77f6fa014c22756c17
Adam Le Doux <aledoux6@gatech.edu>
2017-12-09 20:07:01 -0800

    bitsy font SVGs

--- experiments/bitmap2svg.js
+++ experiments/bitmap2svg.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var font = require('./font_module.js');
 
 console.log("hello");
 
@@ -8,6 +9,58 @@ svg += '<svg width="60" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:xli
 svg += '<rect width="10" height="10" fill="#000"/>' + '\n';
 svg += '</svg>' + '\n';
 
-fs.writeFile("font/svg0.svg", svg);
+function bitmapToSvg( bitmapArray, width, height, filename ) {
+	var svg = "";
+	svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' + '\n';
+	svg += '<svg width="' + (width*10) + '" height="' + (height*10) + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">' + '\n';
+
+	for( var i = 0; i < height; i++ ) {
+		for( var j = 0; j < width; j++ ) {
+			if( bitmapArray[ (i * width ) + j ] == 1 )
+				svg += '<rect x="' + (j*10) + '" y="' + (i*10) + '" width="10" height="10" fill="#000"/>' + '\n';
+		}
+	}
+
+	svg += '</svg>' + '\n';
+
+	fs.writeFile(filename, svg);
+}
+
+/* FONT */
+function charToSvg( char ) {
+	var charData = font.getChar( char );
+	bitmapToSvg( charData, 6, 8, "font/char_" + char.charCodeAt(0) + ".svg" );
+}
+
+function exportFont() {
+	for( var i = 0; i < 256; i++ ) {
+		var char = String.fromCharCode( i );
+		console.log("export " + char);
+		charToSvg( char );
+	}
+}
+// exportFont();
+
+/* CAT */
+var cat_data = 
+[
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,1,0,1,0,0,0,1,
+	0,1,1,1,0,0,0,1,
+	0,1,1,1,0,0,1,0,
+	0,1,1,1,1,1,0,0,
+	0,0,1,1,1,1,0,0,
+	0,0,1,0,0,1,0,0
+];
+// bitmapToSvg( cat_data, 8, 8, "font/char_cat.svg" );
+
+/* ARROW */
+var arrowdata = [
+	1,1,1,1,1,
+	0,1,1,1,0,
+	0,0,1,0,0
+];
+// bitmapToSvg( arrowdata, 5, 3, "font/char_arrow.svg" );
 
 console.log("svg done!");
