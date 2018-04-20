475b5c89d15e7534f676d14f756d8b98f3ee47a5
Adam Le Doux <adamrossledoux@gmail.com>
2018-04-20 08:25:02 -0700

    v4.8 notes

--- editor/shared/script/bitsy.js
+++ editor/shared/script/bitsy.js
@@ -58,7 +58,7 @@ var spriteStartLocations = {};
 /* VERSION */
 var version = {
 	major: 4, // for file format / engine changes
-	minor: 7 // for editor changes and bugfixes
+	minor: 8 // for editor changes and bugfixes
 };
 function getEngineVersion() {
 	return version.major + "." + version.minor;
