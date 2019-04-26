eddc64835a7936c4f8f7223e7003a80923bb5ad9
Adam Le Doux <adamrossledoux@gmail.com>
2019-04-26 08:21:00 -0500

    simplify panel adding + scroll into view

--- editor/shared/script/editor.js
+++ editor/shared/script/editor.js
@@ -32,6 +32,7 @@ X test on my phone
 	- try to sever most direct connections between modules
 	- cur palette change
 	- other UI events?
+	- play mode and edit mode events might be useful, based on some bugs I've seen
 - encapsulate adv dialog logic and inventory logic
 - tool positioning (up and down buttons)
 - bug: during exit drag, they move too far and go off screen
@@ -2579,38 +2580,52 @@ function togglePanelCore(id,visible,doUpdatePrefs=true) {
 }
 
 function togglePanelUI(id,visible) {
-	if( visible ) {
-		if (window.innerHeight > window.innerWidth) {
-			// vertical mode (phone optimized)
-			// just insert at the end for now while I think about the correct way to move cards in this mode
-			var editorContent = document.getElementById("editorContent");
-			var cardElement = document.getElementById(id);
-			editorContent.appendChild(cardElement);
+	// if( visible ) {
+	// 	if (window.innerHeight > window.innerWidth) {
+	// 		// vertical mode (phone optimized)
+	// 		// just insert at the end for now while I think about the correct way to move cards in this mode
+	// 		var editorContent = document.getElementById("editorContent");
+	// 		var cardElement = document.getElementById(id);
+	// 		editorContent.appendChild(cardElement);
 
-			// // TODO 
-			// console.log(cardElement.offsetTop);
-			// editorContent.scrollTop = cardElement.offsetTop;
-		}
-		else {
-			// horizontal mode (desktop optimized)
-			// move panel to the left of the left-most visible panel
-			var editorContent = document.getElementById("editorContent");
-			var otherCards = Array.prototype.slice.call( editorContent.getElementsByClassName("panel") );
-			console.log(otherCards.length);
-			otherCards = otherCards.filter(
-							function(card) {
-								var pos = getElementPosition( card );
-								return card.style.display != "none" && pos.x >= 0;
-							});
-			otherCards = otherCards.sort(
-							function(card1,card2) {
-								return getElementPosition( card1 ).x - getElementPosition( card2 ).x;
-							});
-			editorContent.insertBefore( document.getElementById(id), otherCards[0] );
-		}
+	// 		// update panel
+	// 		document.getElementById(id).style.display = visible ? "inline-block" : "none";
+	// 		cardElement.scrollIntoView();
+	// 	}
+	// 	else {
+	// 		// horizontal mode (desktop optimized)
+	// 		// move panel to the left of the left-most visible panel
+	// 		var editorContent = document.getElementById("editorContent");
+	// 		var otherCards = Array.prototype.slice.call( editorContent.getElementsByClassName("panel") );
+	// 		console.log(otherCards.length);
+	// 		otherCards = otherCards.filter(
+	// 						function(card) {
+	// 							var pos = getElementPosition( card );
+	// 							return card.style.display != "none" && pos.x >= 0;
+	// 						});
+	// 		otherCards = otherCards.sort(
+	// 						function(card1,card2) {
+	// 							return getElementPosition( card1 ).x - getElementPosition( card2 ).x;
+	// 						});
+	// 		editorContent.insertBefore( document.getElementById(id), otherCards[0] );
+
+	// 		// update panel
+	// 		document.getElementById(id).style.display = visible ? "inline-block" : "none";
+	// 	}
+	// }
+
+	if( visible ) {
+		var editorContent = document.getElementById("editorContent");
+		var cardElement = document.getElementById(id);
+		editorContent.appendChild(cardElement);
 	}
-	// update panel
+
 	document.getElementById(id).style.display = visible ? "inline-block" : "none";
+
+	if (visible) {
+		cardElement.scrollIntoView();
+	}
+
 	// update checkbox
 	if (id != "toolsPanel") {
 		document.getElementById(id.replace("Panel","Check")).checked = visible;
