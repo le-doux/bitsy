003b54120439ac0961f8a12fb14b7e3aa01ff62c
Adam Le Doux <adamrossledoux@gmail.com>
2019-05-28 09:49:15 -0500

    resource packager basically working

--- experiments/resource_packager.js
+++ experiments/resource_packager.js
@@ -1,5 +1,28 @@
-console.log("test");
-
 var fs = require("fs");
 
-fs.readFile("../editor/index.html", "utf8", function (err, data) { console.log(data); });
+var resourceFiles = [
+	"../editor/index.html",
+	"../editor/shared/script/font.js",
+];
+
+var resourcePackage = {};
+
+function getFileName(path) {
+	var splitPath = path.split("/");
+	return splitPath[splitPath.length - 1];
+}
+
+for (var i = 0; i < resourceFiles.length; i++) {
+	var path = resourceFiles[i];
+	var fileName = getFileName(path);
+	var result = fs.readFileSync(path, "utf8");
+	resourcePackage[fileName] = result;
+}
+
+var resourceJavascriptFile = "var Resources = " + JSON.stringify(resourcePackage, null, 2) + ";";
+
+fs.writeFile("resources.js", resourceJavascriptFile, function () {});
+
+// console.log(resourcePackage);
+
+console.log("done!");
