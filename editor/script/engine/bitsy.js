701cbf84f8a3be56255748b628fdc952a3a03531
Adam Le Doux <adamrossledoux@gmail.com>
2020-03-22 10:05:55 -0700

    update version number

--- editor/script/engine/bitsy.js
+++ editor/script/engine/bitsy.js
@@ -67,7 +67,7 @@ var spriteStartLocations = {};
 /* VERSION */
 var version = {
 	major: 6, // major changes
-	minor: 4 // smaller changes
+	minor: 5 // smaller changes
 };
 function getEngineVersion() {
 	return version.major + "." + version.minor;
