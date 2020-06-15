b3f1bbd6c4a311a7e7523f514db2aaea223c37eb
Adam Le Doux <adamrossledoux@gmail.com>
2020-06-14 17:54:41 -0700

    generated SVG uses viewBox

--- dev/icon_generator/makeicons.js
+++ dev/icon_generator/makeicons.js
@@ -22,12 +22,12 @@ console.log("*** generating icons ***");
 function drawingToSvg(bitmapArray, width, height, filename) {
 	var svg = "";
 	svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' + '\n';
-	svg += '<svg width="' + (width*10) + '" height="' + (height*10) + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">' + '\n';
+	svg += '<svg viewBox="0 0 ' + (width*10) + ' ' + (height*10) + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">' + '\n';
 
 	for (var i = 0; i < height; i++) {
 		for (var j = 0; j < width; j++) {
 			if (bitmapArray[i][j] == 1) {
-				svg += '<rect x="' + (j*10) + '" y="' + (i*10) + '" width="10" height="10" fill="#000"/>' + '\n';
+				svg += '<rect x="' + (j*10) + '" y="' + (i*10) + '" width="10" height="10" />' + '\n';
 			}
 		}
 	}
