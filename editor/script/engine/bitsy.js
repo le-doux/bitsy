eb4a4eb3db42b02164ae84b5544d262303192949
Adam Le Doux <adamrossledoux@gmail.com>
2021-06-13 10:42:55 -0700

    version # -> 7.3

--- editor/script/engine/bitsy.js
+++ editor/script/engine/bitsy.js
@@ -64,7 +64,7 @@ var spriteStartLocations = {};
 /* VERSION */
 var version = {
 	major: 7, // major changes
-	minor: 2, // smaller changes
+	minor: 3, // smaller changes
 	devBuildPhase: "RELEASE",
 };
 function getEngineVersion() {
