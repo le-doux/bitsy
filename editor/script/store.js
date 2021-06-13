80ede1daf81790ecaca9d9fa4f17daa630e39ac3
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-06-13 15:56:36 -0400

    add storage wrapper supporting fallbacks

---
+++ editor/script/store.js
@@ -0,0 +1,78 @@
+/**
+ * ```js
+ * // call once
+ * Store.init(function onFirstError() {
+ * 	// called once when storage fails;
+ * 	// use to signal lack of storage to user
+ * 	window.alert('Storage is not working!');
+ * });
+ * // call as needed
+ * Store.get('key', 'fallback value');
+ * Store.set('key', 'new value');
+ * ```
+ */
+var Store = {
+	/** storage driver (must implement `getItem`, `setItem`, `removeItem`) */
+	driver: localStorage,
+	/** whether storage has failed */
+	error: false,
+	/**
+	 * Initializes storage mechanism
+	 * @param {(err: Error) => void} [onFirstError] called once when storage fails;
+	 * use to signal lack of storage to user
+	 */
+	init: function (onFirstError) {
+		this.onFirstError = function(err) {
+			this.error = true;
+			console.warn('Storage error', err);
+			if (onFirstError) onFirstError(err);
+		};
+		try {
+			// test that storage driver is available and working
+			this.driver.setItem('_test_key_', 'test');
+			this.driver.getItem('_test_key_');
+			this.driver.removeItem('_test_key_');
+		} catch (err) {
+			this.onFirstError(err);
+		}
+	},
+	/**
+	 * @param {string} name storage key
+	 * @param {any} [fallback] value returned if storage fails, or value for key is undefined
+	 * @returns {any} stored value for key, or fallback value
+	 */
+	get: function (name, fallback) {
+		try {
+			var value = this.driver.getItem(name);
+			if (typeof value === 'string') {
+				try {
+					return JSON.parse(value);
+				} catch {
+					return value;
+				}
+			}
+		} catch (err) {
+			if (!this.error && this.onFirstError) {
+				this.onFirstError(err);
+			}
+		}
+		return fallback;
+	},
+	/**
+	 * @param {string} name storage key
+	 * @param {any} [value] value to store (use `undefined` to clear)
+	 */
+	set: function (name, value) {
+		try {
+			if (value === undefined) {
+				this.driver.removeItem(name);
+			} else {
+				this.driver.setItem(name, JSON.stringify(value));
+			}
+		} catch (err) {
+			if (!this.error && this.onFirstError) {
+				this.onFirstError(err);
+			}
+		}
+	},
+};
