68998e5268b5a934628214070c41c7a5dc9aa808
Adam Le Doux <adamrossledoux@gmail.com>
2021-06-17 08:44:09 -0700

    test creating item entries

--- editor/script/find.js
+++ editor/script/find.js
@@ -1,34 +1,49 @@
 function FindTool(options) {
-	var searchGroup = createGroupContainer();
+	// var searchGroup = createGroupContainer();
 
-	searchGroup.appendChild(createLabel({
-		icon: "search",
-	}));
+	// searchGroup.appendChild(createLabel({
+	// 	icon: "search",
+	// }));
 
-	searchGroup.appendChild(createTextInput({
-		placeholder: "search by name",
-	}));
+	// searchGroup.appendChild(createTextInput({
+	// 	placeholder: "search by name",
+	// }));
 
-	options.mainElement.appendChild(searchGroup);
+	// options.mainElement.appendChild(searchGroup);
 
-	var filterTabs = createTabs({
-		name: "findFilter",
-		tabs: [
-			{ text: "all", value: "all", icon: "game_data", },
-			{ text: "in room", value: "in_room", icon: "set_exit_location", },
-			{ text: "avatar", value: "avatar", icon: "avatar", },
-			{ text: "tile", value: "tile", icon: "tile", },
-			{ text: "sprite", value: "sprite", icon: "sprite", },
-			{ text: "item", value: "item", icon: "item", },
-			{ text: "room", value: "room", icon: "room", },
-			{ text: "colors", value: "colors", icon: "colors", },
-			{ text: "dialog", value: "dialog", icon: "dialog", },
-		],
-	});
+	// var filterTabs = createTabs({
+	// 	name: "findFilter",
+	// 	tabs: [
+	// 		{ text: "all", value: "all", icon: "game_data", },
+	// 		{ text: "in room", value: "in_room", icon: "set_exit_location", },
+	// 		{ text: "avatar", value: "avatar", icon: "avatar", },
+	// 		{ text: "tile", value: "tile", icon: "tile", },
+	// 		{ text: "sprite", value: "sprite", icon: "sprite", },
+	// 		{ text: "item", value: "item", icon: "item", },
+	// 		{ text: "room", value: "room", icon: "room", },
+	// 		{ text: "colors", value: "colors", icon: "colors", },
+	// 		{ text: "dialog", value: "dialog", icon: "dialog", },
+	// 	],
+	// });
 
-	filterTabs.classList.add("bitsy-menu-group");
+	// filterTabs.classList.add("bitsy-menu-group");
 
-	options.mainElement.appendChild(filterTabs);
+	// options.mainElement.appendChild(filterTabs);
 
-	options.mainElement.appendChild(createScrollview());
+	var thumbnailScrollview = createScrollview();
+
+	options.mainElement.appendChild(thumbnailScrollview);
+
+	function createOnClickHandler(id) {
+		return function() {
+			paintTool.selectDrawing(new DrawingId(TileType.Item, id));
+			on_paint_item_ui_update();
+		}
+	}
+
+	for (var id in item) {
+		var itemIcon = createIconElement("item");
+		itemIcon.onclick = createOnClickHandler(id);
+		thumbnailScrollview.appendChild(itemIcon);
+	}
 }
