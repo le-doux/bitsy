f44b8000ec8b5cbe629d091eee38b79f48733664
Adam Le Doux <adamrossledoux@gmail.com>
2021-06-19 08:54:44 -0700

    filters and tooltips

--- editor/script/find.js
+++ editor/script/find.js
@@ -1,8 +1,12 @@
 function FindTool(options) {
 	var categoryDefinitions = [
 		{
+			name: "avatar",
 			icon: "avatar",
 			getIdList: function() { return ["A"]; },
+			getDisplayName: function(id) {
+				return localization.GetStringOrFallback("avatar_label", "avatar");
+			},
 			openTool: function(id) {
 				paintTool.selectDrawing(new DrawingId(TileType.Avatar, id));
 				on_paint_avatar_ui_update();
@@ -10,8 +14,17 @@ function FindTool(options) {
 			},
 		},
 		{
+			name: "tile",
 			icon: "tile",
 			getIdList: function() { return sortedTileIdList(); },
+			getDisplayName: function(id) {
+				if (tile[id].name) {
+					return tile[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("tile_label", "tile") + " " + id;
+				}
+			},
 			openTool: function(id) {
 				paintTool.selectDrawing(new DrawingId(TileType.Tile, id));
 				on_paint_tile_ui_update();
@@ -19,11 +32,20 @@ function FindTool(options) {
 			},
 		},
 		{
+			name: "sprite",
 			icon: "sprite",
 			getIdList: function() {
 				var idList = sortedSpriteIdList();
 				idList.splice(idList.indexOf("A"), 1);
 				return idList;
+			},
+			getDisplayName: function(id) {
+				if (sprite[id].name) {
+					return sprite[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("sprite_label", "sprite") + " " + id;
+				}
 			},
 			openTool: function(id) {
 				paintTool.selectDrawing(new DrawingId(TileType.Sprite, id));
@@ -32,8 +54,17 @@ function FindTool(options) {
 			},
 		},
 		{
+			name: "item",
 			icon: "item",
 			getIdList: function() { return sortedItemIdList(); },
+			getDisplayName: function(id) {
+				if (item[id].name) {
+					return item[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("item_label", "item") + " " + id;
+				}
+			},
 			openTool: function(id) {
 				paintTool.selectDrawing(new DrawingId(TileType.Item, id));
 				on_paint_item_ui_update();
@@ -41,30 +72,63 @@ function FindTool(options) {
 			},
 		},
 		{
+			name: "room",
 			icon: "room",
 			getIdList: function() { return sortedRoomIdList(); },
+			getDisplayName: function(id) {
+				if (room[id].name) {
+					return room[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("room_label", "room") + " " + id;
+				}
+			},
 			openTool: function(id) {
 				selectRoom(id);
 				showPanel("roomPanel", "findPanel");
 			},
 		},
 		{
+			name: "colors",
 			icon: "colors",
 			getIdList: function() { return sortedPaletteIdList(); },
+			getDisplayName: function(id) {
+				if (palette[id].name) {
+					return palette[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("palette_label", "palette") + " " + id;
+				}
+			},
 			openTool: function(id) {
 				paletteTool.Select(id);
 				showPanel("colorsPanel", "findPanel");
 			},
 		},
 		{
+			name: "dialog",
 			icon: "dialog",
 			getIdList: function() { return [titleDialogId].concat(sortedDialogIdList()); },
+			getDisplayName: function(id) {
+				if (id === titleDialogId) {
+					return titleDialogId; // todo : localize
+				}
+				else if (dialog[id].name) {
+					return dialog[id].name;
+				}
+				else {
+					return localization.GetStringOrFallback("dialog_label", "dialog") + " " + id;
+				}
+			},
 			openTool: function(id) {
 				openDialogTool(id);
 				showPanel("dialogPanel", "findPanel");
 			},
 		},
 	];
+
+	var curFilter = "all";
+	var curSearchText = "";
 
 	var searchGroup = createGroupContainer();
 
@@ -74,28 +138,41 @@ function FindTool(options) {
 
 	searchGroup.appendChild(createTextInput({
 		placeholder: "find by name or id",
+		onchange: function(e) {
+			curSearchText = e.target.value;
+			GenerateItems();
+		},
 	}));
 
 	options.mainElement.appendChild(searchGroup);
 
-	var filterTabs = createTabs({
+	var filterTabList = [
+		{ text: "all", value: "all", icon: "game_data", },
+	];
+
+	for (var i = 0; i < categoryDefinitions.length; i++) {
+		var category = categoryDefinitions[i];
+
+		filterTabList.push({
+			text: category.name,
+			value: category.name,
+			icon: category.icon,
+		});
+	}
+
+	var filterSelect = createTabs({
 		name: "findFilter",
-		tabs: [
-			{ text: "all", value: "all", icon: "game_data", },
-			{ text: "in room", value: "in_room", icon: "set_exit_location", },
-			{ text: "avatar", value: "avatar", icon: "avatar", },
-			{ text: "tile", value: "tile", icon: "tile", },
-			{ text: "sprite", value: "sprite", icon: "sprite", },
-			{ text: "item", value: "item", icon: "item", },
-			{ text: "room", value: "room", icon: "room", },
-			{ text: "colors", value: "colors", icon: "colors", },
-			{ text: "dialog", value: "dialog", icon: "dialog", },
-		],
+		value: curFilter,
+		tabs: filterTabList,
+		onclick: function(e) {
+			curFilter = e.target.value;
+			GenerateItems();
+		},
 	});
 
-	filterTabs.classList.add("bitsy-menu-group");
+	filterSelect.classList.add("bitsy-menu-group");
 
-	options.mainElement.appendChild(filterTabs);
+	options.mainElement.appendChild(filterSelect);
 
 	var scrollviewDiv = document.createElement("div");
 	scrollviewDiv.classList.add("bitsy-menu-scrollview");
@@ -118,14 +195,22 @@ function FindTool(options) {
 		for (var i = 0; i < categoryDefinitions.length; i++) {
 			var category = categoryDefinitions[i];
 
-			var idList = category.getIdList()
+			if (curFilter === "all" || curFilter === category.name) {
+				var idList = category.getIdList()
 
-			for (var j = 0; j < idList.length; j++) {
-				var id = idList[j];
-				var icon = createIconElement(category.icon);
-				icon.onclick = createOnClick(category, id);
-				icon.title = id;
-				scrollcontentDiv.appendChild(icon);
+				for (var j = 0; j < idList.length; j++) {
+					var id = idList[j];
+					var displayName = category.getDisplayName(id);
+					var isSearchTextInName = (curSearchText === undefined || curSearchText === null ||
+						curSearchText.length <= 0 || displayName.indexOf(curSearchText) != -1);
+
+					if (isSearchTextInName) {
+						var icon = createIconElement(category.icon);
+						icon.onclick = createOnClick(category, id);
+						icon.title = displayName;
+						scrollcontentDiv.appendChild(icon);
+					}
+				}
 			}
 		}
 	}
