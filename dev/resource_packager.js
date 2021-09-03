a79ad3304cd82c634c9a89d83795ff56653a5a91
Adam Le Doux <adamrossledoux@gmail.com>
2021-09-02 18:17:53 -0700

    update resource packager

--- dev/resource_packager.js
+++ dev/resource_packager.js
@@ -21,7 +21,6 @@ var resourceFiles = [
 	"../editor/script/engine/font.js",
 	"../editor/script/engine/dialog.js",
 	"../editor/script/engine/script.js",
-	"../editor/script/engine/color_util.js",
 	"../editor/script/engine/renderer.js",
 	"../editor/script/engine/transition.js",
 ];
