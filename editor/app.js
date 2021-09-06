d6d888dc25401db41b145cc17741df65082de0a1
Adam Le Doux <adamrossledoux@gmail.com>
2021-09-06 15:44:28 -0700

    turn off service worker

--- editor/app.js
+++ editor/app.js
@@ -1,18 +1,30 @@
-if ("serviceWorker" in navigator) {
-	console.log("service workers exist :)");
+// service worker turned off for now
+// if ("serviceWorker" in navigator) {
+// 	console.log("service workers exist :)");
 
-	navigator.serviceWorker.register("sw.js").then(function (reg){
-		if (reg.installing) {
-			console.log("service worker installing...");
-		}
-		else if (reg.waiting) {
-			console.log("service worker installed!");
-		}
-		else if (reg.active) {
-			console.log("service worker active :)");
-		}
-	}).catch(function(error) {
-		console.log("service worker registration failed :(");
-		console.log("service worker error: " + error);
-	});
-}
+// 	var isRefreshing = false;
+// 	navigator.serviceWorker.addEventListener("controllerchange", function(event) {
+// 		if (!isRefreshing) {
+// 			console.log("force refresh to update service worker");
+
+// 			// refresh the page when a new version finishes installing
+// 			isRefreshing = true;
+// 			window.location.reload();
+// 		}
+// 	});
+
+// 	navigator.serviceWorker.register("sw.js").then(function (reg){
+// 		if (reg.installing) {
+// 			console.log("service worker installing...");
+// 		}
+// 		else if (reg.waiting) {
+// 			console.log("service worker installed!");
+// 		}
+// 		else if (reg.active) {
+// 			console.log("service worker active :)");
+// 		}
+// 	}).catch(function(error) {
+// 		console.log("service worker registration failed :(");
+// 		console.log("service worker error: " + error);
+// 	});
+// }
--- editor/sw.js
+++ editor/sw.js
@@ -2,89 +2,20 @@
 SERVICE WORKER
 */
 
-var cacheVersionId = "bitsy-cache-v7.9";
+var cacheVersionId = "bitsy-cache-v7.9.5";
 
-self.addEventListener("install", function(event) {
-	event.waitUntil(
-		caches.open(cacheVersionId).then(function(cache) {
-			// new versions should install immediately
-			self.skipWaiting();
-
-			return cache.addAll([
-				"./app.js",
-				"./font/bitsy_ascii_small.ttf",
-				"./font/google-material-icons.woff2",
-				"./font/google-nunito-latin.woff2",
-				"./font/google-nunito-latinext.woff2",
-				"./font/google-nunito-vietnamese.woff2",
-				"./icons/bitsy.hqx",
-				"./icons/bitsy.icns",
-				"./icons/bitsy.ico",
-				"./icons/bitsy.png",
-				"./image/bitsy-itch-cover.png",
-				"./image/bitsy.icns",
-				"./image/cat.png",
-				"./image/cat.svg",
-				"./image/cat2.png",
-				"./image/cat5.png",
-				"./image/down_arrow.svg",
-				"./index.html",
-				"./manifest.json",
-				"./script/color_picker.js",
-				"./script/dialog_editor.js",
-				"./script/editor.js",
-				"./script/engine/bitsy.js",
-				"./script/engine/dialog.js",
-				"./script/engine/font.js",
-				"./script/engine/renderer.js",
-				"./script/engine/script.js",
-				"./script/engine/system.js",
-				"./script/engine/transition.js",
-				"./script/event_manager.js",
-				"./script/exporter.js",
-				"./script/find.js",
-				"./script/generated/resources.js",
-				"./script/gif.js",
-				"./script/icons.js",
-				"./script/inventory.js",
-				"./script/localization.js",
-				"./script/menu.js",
-				"./script/paint.js",
-				"./script/palette.js",
-				"./script/room.js",
-				"./script/room_markers.js",
-				"./script/store.js",
-				"./script/thumbnail.js",
-				"./script/util.js",
-				"./style/aboutToolStyle.css",
-				"./style/bitsyEditorStyle.css",
-				"./style/colorsToolStyle.css",
-				"./style/dataToolStyle.css",
-				"./style/dialogToolStyle.css",
-				"./style/exitsToolStyle.css",
-				"./style/gifToolStyle.css",
-				"./style/googleNunito.css",
-				"./style/paintToolStyle.css",
-				"./style/settingsToolStyle.css",
-			]);
-		})
-	);
+// NOTE: turning off service worker until I can figure out how to get them to update properly
+// self destroying service worker from: https://github.com/NekR/self-destroying-sw
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
-	// if the object exists in the cache, return it, otherwise try the network
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
-});
-
-var isRefreshing = false;
-self.addEventListener("controllerchange", function(event) {
-	if (!isRefreshing) {
-		// refresh the page when a new version finishes installing
-		isRefreshing = true;
-		window.location.reload();
-	}
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
 });
