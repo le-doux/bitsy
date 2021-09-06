a3902eff0699ce9a47e7ab88448ce164d1090150
Adam Le Doux <adamrossledoux@gmail.com>
2021-09-06 14:57:02 -0700

    update resources and version to 7.9

--- editor/script/engine/bitsy.js
+++ editor/script/engine/bitsy.js
@@ -63,7 +63,7 @@ var spriteStartLocations = {};
 /* VERSION */
 var version = {
 	major: 7, // major changes
-	minor: 8, // smaller changes
+	minor: 9, // smaller changes
 	devBuildPhase: "RELEASE",
 };
 function getEngineVersion() {
