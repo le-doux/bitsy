bb675e830e7394afcfff00b9970339c7ce565060
David O'Rourke <david-c14@users.noreply.github.com>
2022-07-08 08:42:07 +0100

    Update font.js
    Read font data for the size of the character, no the default font size

--- editor/script/engine/font.js
+++ editor/script/engine/font.js
@@ -202,7 +202,7 @@ function Font(fontData) {
 					}
 
 					curCharLineCount++;
-					if (curCharLineCount >= height) {
+					if (curCharLineCount >= chardata[curCharCode].height) {
 						isReadingChar = false;
 					}
 				}
