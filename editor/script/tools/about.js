f053cb232d9d8f9f40f2c7d4de192276c3372ac6
Adam Le Doux <adamrossledoux@gmail.com>
2022-08-21 10:24:35 -0700

    about-tool: remove broken page history feature, make "open in tab" more robust, and make testing with localhost easier

--- editor/script/tools/about.js
+++ editor/script/tools/about.js
@@ -1,32 +1,20 @@
 /* ABOUT / DOCUMENTATION TOOL */
+var aboutFlags = {
+	isLocalhost: false, // flag for local debugging with a server running
+};
 
-// todo : only valid for development - need a real root later
-var docsRoot = window.location.href.replace(/\/?((index|editor)\.html)?$/, '/docs/');
-
-var isUpdatingHistory = true;
-var aboutHistory = [];
-var aboutHistoryIndex = -1;
-
-function setAboutPage(pagePath) {
-	var src = new URL(pagePath.includes('.html') ? pagePath : `${pagePath}/index.html`, docsRoot).href;
-	docsFrame.src = src;
-}
-
-function onAboutLoad() {
-	// update history
-	docsFrame = document.getElementById('docsFrame');
-	var url = docsFrame.src;
-
-	if (isUpdatingHistory && url.length > 0) {
-		// track history
-		if (aboutHistoryIndex >= 0 && aboutHistoryIndex < (aboutHistory.length - 1)) {
-			aboutHistory.splice(aboutHistoryIndex + 1);
-		}
-		aboutHistory.push(url);
-		aboutHistoryIndex = (aboutHistory.length - 1);
+function getDocsRoot() {
+	if (aboutFlags.isLocalhost) {
+		return "http://localhost:3000/docs/";
 	}
 
-	isUpdatingHistory = true;
+	return window.location.href.replace(/index\.html/, '/docs/');
+}
+
+function setAboutPage(pagePath) {
+	var docsFrame = document.getElementById('docsFrame');
+	var src = new URL(pagePath.includes('.html') ? pagePath : `${pagePath}/index.html`, getDocsRoot()).href;
+	docsFrame.src = src;
 }
 
 function initAbout() {
@@ -38,31 +26,26 @@ function showAbout(pagePath, insertNextToId) {
 	showPanel("aboutPanel", insertNextToId);
 }
 
-function aboutBack() {
-	if (aboutHistory.length <= 0) {
-		return;
-	}
-
-	aboutHistoryIndex = Math.max(0, aboutHistoryIndex - 1);
-
-	isUpdatingHistory = false;
-	var docsFrame = document.getElementById('docsFrame');
-	docsFrame.src = aboutHistory[aboutHistoryIndex];
-}
-
-function aboutForward() {
-	if (aboutHistory.length <= 0) {
-		return;
-	}
-
-	aboutHistoryIndex = Math.min(aboutHistory.length - 1, aboutHistoryIndex + 1);
-
-	isUpdatingHistory = false;
-	var docsFrame = document.getElementById('docsFrame');
-	docsFrame.src = aboutHistory[aboutHistoryIndex];
-}
-
-// todo : this doesn't work if the user navigates *within* the iframe
 function aboutOpenTab() {
-	window.open(docsFrame.src);
+	var docsFrame = document.getElementById('docsFrame');
+
+	// use the `src` attribute of the docs frame as the fallback value for the page url
+	// in case we're unable to access the actual current location of the frame:
+	// this will happen if we try to make a cross-origin request, for example:
+	// - if we're testing locally using localhost
+	// - OR if the docs link to a location outside of the bitsy editor's domain
+	var docsPageUrl = docsFrame.src;
+
+	try {
+		// attempt to get the page URL from the docs frame location, since this will be more accurate:
+		// for example: if the user clicks links within the docs frame *this* value will update to
+		// contain the new location of the frame, but the `src` attribute will *not* update
+		docsPageUrl = docsFrame.contentWindow.location.href;
+	}
+	catch (err) {
+		// todo : use bitsyLog()?
+		console.log("could not read docs page location, falling back to src: " + docsPageUrl, err);
+	}
+
+	window.open(docsPageUrl);
 }
