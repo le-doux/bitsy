143577dda71d730cc2bbf395567a9cfe14606d99
pancelor <pancelor@gmail.com>
2023-12-31 03:31:54 -0800

    revert "make sure ClearCache() deletes tile memory"
    fixes #250
    
    my ClearCache change broke the editor; the reset-room code
    that gets called when edits are made is too complex for me
    to reason about. I added some logging to ClearCache (after making this
    commit) and noticed that ClearCache is called 4 separate times
    every time you make an edit in the sprite Paint tool -- seems odd
    
    stepping back, I think that the room should not reset itself on
    every stroke of the Paint tool. but I'd rather not break anything
    else so I'm not going to dig into it on my own.
    
    anyway, my bad! this commit undoes my previous commit, which fixes #250
    
    but now #230 is back, and I don't know how to fix it

--- editor/script/engine/renderer.js
+++ editor/script/engine/renderer.js
@@ -135,7 +135,12 @@ this.GetFrameCount = function(drawingId) {
 // todo : forceReset option is hacky?
 this.ClearCache = function(forceReset) {
 	if (forceReset === undefined || forceReset === true) {
-		bitsy.deleteAllTiles();
+		for (var cacheId in drawingCache.render) {
+			var tiles = drawingCache.render[cacheId];
+			for (var i = 0; i < tiles.length; i++) {
+				bitsy.delete(tiles[i]);
+			}
+		}
 	}
 
 	drawingCache.render = {};
